e3c2058bc481dd0125eb90a9263265cc5e8d99a7
Francisco Javier Torres Ibañez <kisco1984@gmail.com>
2015-12-10 13:14:29 +0100

    Add list divider decorator

---
+++ presentation/src/main/java/com/github/mytravelsapp/presentation/view/components/SimpleDividerItemDecoration.java
@@ -0,0 +1,47 @@
+package com.github.mytravelsapp.presentation.view.components;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.github.mytravelsapp.R;
+
+/**
+ * Divider decorator for list.
+ *
+ * @author fjtorres
+ */
+public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private Drawable mDivider;
+
+
+    /**
+     * Custom divider will be used
+     */
+    public SimpleDividerItemDecoration(Context context) {
+        mDivider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
