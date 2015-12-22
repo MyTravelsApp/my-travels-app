66044d4bea9de6b193d36a962f54728101535daa
Francisco Javier Torres Ibañez <kisco1984@gmail.com>
2015-12-22 21:52:46 +0100

    Add color below swipe row and optimize divider.

--- presentation/src/main/java/com/github/mytravelsapp/presentation/view/components/TravelTouchHelperCallback.java
+++ presentation/src/main/java/com/github/mytravelsapp/presentation/view/components/TravelTouchHelperCallback.java
@@ -1,8 +1,12 @@
 package com.github.mytravelsapp.presentation.view.components;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
+import com.github.mytravelsapp.R;
 import com.github.mytravelsapp.presentation.view.adapter.TravelAdapter;
 
 /**
@@ -12,7 +16,7 @@ public class TravelTouchHelperCallback extends ItemTouchHelper.SimpleCallback {
     private final TravelAdapter adapter;
 
     public TravelTouchHelperCallback(final TravelAdapter pAdapter) {
-        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT);
         this.adapter = pAdapter;
     }
 
@@ -25,5 +29,24 @@ public class TravelTouchHelperCallback extends ItemTouchHelper.SimpleCallback {
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
         adapter.remove(viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            // Get RecyclerView item from the ViewHolder
+            View itemView = viewHolder.itemView;
+
+            Paint p = new Paint();
+            if (dX > 0) {
+                p.setColor(recyclerView.getResources().getColor(R.color.colorAccent));
+
+                // Draw Rect with varying right side, equal to displacement dX
+                c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                        (float) itemView.getBottom(), p);
+            }
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     }
 }
