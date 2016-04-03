433e47c2ac742166a956501e8a86dee1b5894af2
Francisco Javier Torres Ibañez <kisco1984@gmail.com>
2016-04-03 20:22:50 +0200

    Add generic error message and generic background callback.

--- presentation/src/main/java/com/github/mytravelsapp/presentation/view/View.java
+++ presentation/src/main/java/com/github/mytravelsapp/presentation/view/View.java
@@ -11,4 +11,9 @@ public interface View {
      * Get a {@link android.content.Context}.
      */
     Context getViewContext();
+
+    /**
+     * Show a generic error message in the view.
+     */
+    void showGenericError ();
 }
